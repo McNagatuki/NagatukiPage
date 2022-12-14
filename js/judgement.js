3cc89667eaef62cc747952c36111361fd321a570
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2022-12-14 22:39:58 +0900

    first commit

---
+++ js/judgement.js
@@ -0,0 +1,17 @@
+// window.onload = function() {
+function judge() {
+    //ここにJavaScriptのコードを記述
+    const targetMinecraft = document.getElementsByClassName("minecraft");
+    const targetNeet = document.getElementsByClassName("neet");
+
+    const startDay = new Date("2022-12-14T08:00:00");
+    const today = new Date()
+
+    const ms = today.getTime() - startDay.getTime();
+    const days = Math.floor(ms / (1000*60*60*24));
+
+    const minecraftStyleDisplay = (ms % 2 == 0) ? "" : "none";
+    const neetStyleDisplay = (ms % 2 == 0) ? "none" : "";
+    Array.prototype.forEach.call(targetMinecraft, e => e.style.display = minecraftStyleDisplay);
+    Array.prototype.forEach.call(targetNeet, e => e.style.display = neetStyleDisplay);
+}
